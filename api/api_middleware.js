ebcdd4d86135971ffc42fa34621884e657d33b8f
Eric Jackson <ejackson@ashevillenc.gov>
2019-01-14 10:27:25 -0500

    Added check for superuser

--- api/api_middleware.js
+++ api/api_middleware.js
@@ -1,9 +1,35 @@
-
+const cache = require('coa-web-cache');
+const getDbConnection = require('../common/db');
+const logger = require('../common/logger');
 
 const middlewares = [
-  function sample(req, res, next) {
-    // You can insert middleware code here, or just delete this function.
-    next();
+  function checkSuperuser(req, res, next) { // Get superuser status
+    if (req.session && req.session.id) {
+      cache.get(req.session.id)
+        .then((cData) => {
+          let user = {};
+          if (cData && cData.user) ({ user } = cData);
+          if (user.id && user.superuser === undefined) {
+            const conn = getDbConnection('reviews');
+            return conn
+              .query(`SELECT * FROM reviews.superusers WHERE emp_id = ${user.id} limit 1`)
+              .then((result) => {
+                if (result.rows.length === 1) {
+                  user.superuser = result.rows[0].is_superuser !== 0;
+                  if (user.superuser) logger.warn(`Superuser login by ${user.email}'`);
+                }
+                const cDataNew = Object.assign({}, cData, { user });
+                return cache.store(req.session.id, cDataNew);
+              })
+              .then(() => {
+                next();
+                return (Promise.resolve(null));
+              });
+          }
+          next();
+          return (Promise.resolve(null));
+        });
+    } else next();
   },
 ];
 
--- api/db_configs.js
+++ api/db_configs.js
@@ -1,1 +1,11 @@
-module.exports = {};
+module.exports = {
+  reviews: {
+    db_type: 'pg',
+    host: process.env.reviews_host,
+    user: process.env.reviews_user,
+    password: process.env.reviews_password,
+    database: process.env.reviews_database,
+    port: 5432,
+    ssl: false,
+  },
+};
